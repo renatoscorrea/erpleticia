301592f53d5c74c64f0884c336574c227d9b906a
renatoscorrea <renato.s.correa@bol.com.br>
2024-10-02 12:58:55 -0300

    Init.php
    init.php mudanças

--- ERPLeticia/init.php
+++ ERPLeticia/init.php
@@ -1,8 +1,6 @@
-<html>
-  <head>
-    <title></title>
-    <meta content="">
-    <style></style>
-  </head>
-  <body></body>
-</html>
+<?php
+
+if(version_compare(PHP_VERSION, '7.1.1') == -1)
+{
+    die('The minimum version required for PHP is 7.1.0');
+}
