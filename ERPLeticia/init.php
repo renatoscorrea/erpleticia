110ba50c0ceb5f67157f417aef47b20279e76e2c
renatoscorrea <renato.s.correa@bol.com.br>
2024-10-02 13:15:36 -0300

    Atualização
    atualização init.php

--- ERPLeticia/init.php
+++ ERPLeticia/init.php
@@ -4,3 +4,8 @@ if(version_compare(PHP_VERSION, '7.1.1') == -1)
 {
     die('The minimum version required for PHP is 7.1.0');
 }
+
+if (!file_exists('app/config/application.ini'))
+{
+    die('Application configuration file not found');
+}
